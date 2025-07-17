f901788e0457bf5022444091ed1a25b41c9b857d
eric406-yeboah <ericusyeboah406@gmail.com>
2025-07-17 13:01:54 +0000

    contact

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -2,8 +2,14 @@
 const home = (req, res) => {
     res.render("index", { activePage: "home" });
   };
+
+const contact = (req, res) => {
+    res.render("contact", { activePage: "contact" });
+  };
+  
   
 module.exports = {
-    home
+    home,
+    contact
   
 }
--- routes/frontendRoutes.js
+++ routes/frontendRoutes.js
@@ -1,7 +1,8 @@
-const {home  } = require('../controllers/frontendController')
+const {home,contact  } = require('../controllers/frontendController')
 const express = require('express')
 const router = express.Router()
 
 router.get('/', home)
+router.get('/contact',contact)
 
 module.exports = router
